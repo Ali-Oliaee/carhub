2063e00efdfb07de5ad960e84808b0f6a28d102f
ali <alioliaee31@gmail.com>
2026-02-08 17:54:54 +0330

    feat: update blog controller methods

--- BE/src/modules/blog/controllers/blog.controller.ts
+++ BE/src/modules/blog/controllers/blog.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { BlogService } from '../services/blog.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Blog')
 @Controller('blogs')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
@@ -8,5 +18,25 @@ export class BlogController {
   @Get()
   findAll() {
     return this.blogService.blogList();
+  }
+
+  @Get(':id')
+  findById(@Param() id: string) {
+    return id;
+  }
+
+  @Post()
+  create(@Body() body: { name: string }) {
+    return body?.name ?? 'Hi';
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string) {
+    return id;
+  }
+
+  @Put(':id')
+  update(@Param() id: string) {
+    return id;
   }
 }
